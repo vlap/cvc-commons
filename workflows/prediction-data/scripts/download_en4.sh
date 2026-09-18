1955bad3176ff6eb569df0f56393e33fa069cff6
vlap <volant1987@gmail.com>
2026-09-18 23:15:31 +0200

    feat: trigger Intake Agent for download_en4.sh

--- workflows/prediction-data/scripts/download_en4.sh
+++ workflows/prediction-data/scripts/download_en4.sh
@@ -9,4 +9,4 @@ set -euo pipefail
 module load netcdf/4.7
 curl -o ./data/en4_raw.tar.gz "https://www.metoffice.gov.uk/hadobs/en4/data/en4-2.1.1.tar.gz"
 tar -xzf ./data/en4_raw.tar.gz -C ./data/
-cdo -f nc copy ./data/en4_raw/*.gde ./data/en4/en4_formatted.nc
+cdo -f nc copy ./data/en4_raw/*.gde ./data/en4/en4_formatted.nc# Intake Agent Test: Added comment for triggering
